7c4007841992a1e1bb954cd660c49b4b9d8d38a2
Tom Exceed <roman.exceed@gmail.com>
2019-04-22 17:20:31 +0300

    test

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-app.get('/index', function(req, res) {
-    res.render('index');
+var port = process.env.PORT || 8080,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
 });
-app.listen(8080)
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
